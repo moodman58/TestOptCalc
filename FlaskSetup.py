8f07dc2c725f18f82bce0288c093f63b8a885b66
moodman58 <mehmoodumar281@gmail.com>
2024-01-09 12:07:34 -0500

    Update FlaskSetup.py

--- FlaskSetup.py
+++ FlaskSetup.py
@@ -2,7 +2,6 @@ from flask import Flask, request, render_template, url_for, redirect, session, j
 from src.main import flight_time  # Adjust import according to your project structure
 
 app = Flask(__name__)
-app.secret_key = "F5P0R1D"
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
@@ -20,29 +19,12 @@ def index():
         session["optimal_batteries"] = float(optimal_batteries)
         session["time_of_flight"] = float(time_of_flight)
         # Pass the results to the template
-        return redirect(url_for('results'))
+       return render_template('results.html', optimal_batteries=optimal_batteries, time_of_flight=time_of_flight)
     else:
         return render_template('index.html')
 
-@app.route('/results', methods=['POST', 'GET'])
-def results():
-    if request.method == 'POST':
-        motor_file = request.form['motor_file']
-        battery_file = request.form['battery_file']
-        drone_weight = request.form['drone_weight']
-        desired_velocity = request.form['desired_velocity']
-        desired_altitude = request.form['desired_altitude']
 
-        # Create an instance of flight_time and call optimization
-        flight_time_instance = flight_time(motor_file, battery_file, drone_weight, desired_velocity, desired_altitude)
-        optimal_batteries = flight_time_instance.optimization()
-        time_of_flight = flight_time_instance.hover_phase(flight_time_instance.optimization())
-        return render_template('results.html', optimal_batteries=optimal_batteries, time_of_flight=time_of_flight)
-    else:
-        optimal_batteries = session['optimal_batteries']
-        time_of_flight = session['time_of_flight']
-        return render_template('results.html', optimal_batteries=optimal_batteries, time_of_flight=time_of_flight)
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
 
     
